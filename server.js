b34721ed3559d277fa84a633b217ce37e6a27019
Shraddhasaini <shraddhasaini99@gmail.com>
2020-05-08 17:20:22 +0530

    SERVER

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 const Hapi = require("@hapi/hapi");
@@ -8,31 +7,31 @@ const Routes = require("./lib/routes");
 const Models = require("./lib/models/");
 
 const init = async () => {
- const server = new Hapi.Server({ port: Settings.port });
+  const server = new Hapi.Server({ port: Settings.port });
 
- await server.register([require("@hapi/vision"), require("@hapi/inert")]);
+  await server.register([require("@hapi/vision"), require("@hapi/inert")]);
 
- server.views({
-   engines: { pug: require("pug") },
-   path: Path.join(__dirname, "lib/views"),
-   compileOptions: {
-     pretty: false
-   },
-   isCached: Settings.env === "production"
- });
+  server.views({
+    engines: { pug: require("pug") },
+    path: Path.join(__dirname, "lib/views"),
+    compileOptions: {
+      pretty: false
+    },
+    isCached: Settings.env === "production"
+  });
 
- // Add routes
- server.route(Routes);
+  // Add routes
+  server.route(Routes);
 
- await Models.sequelize.sync();
+  await Models.sequelize.sync();
 
- await server.start();
- console.log(`Server running at: ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on("unhandledRejection", err => {
- console.log(err);
- process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
