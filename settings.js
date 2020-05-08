efa01e367ed6d4597658bc6c45e29c1f68039aa1
Shraddhasaini <shraddhasaini99@gmail.com>
2020-05-08 17:20:54 +0530

    settings

--- settings.js
+++ settings.js
@@ -1,24 +1,20 @@
-// This will load our .env file and add the values to process.env,
-// IMPORTANT: Omit this line if you don't want to use this functionality
-require('dotenv').config({silent: true});
-
-//const dbSettings = Settings[Settings.env].db;
+require("dotenv").config({ silent: true });
 
 module.exports = {
   port: process.env.PORT || 3000,
-  env: process.env.ENV || 'development',
+  env: process.env.NODE_ENV || "development",
 
   // Environment-dependent settings
   development: {
     db: {
-      dialect: 'sqlite',
-      storage: ':memory:'
+      dialect: "sqlite",
+      storage: ":memory:"
     }
   },
   production: {
     db: {
-      dialect: 'sqlite',
-      storage: 'db/database.sqlite'
+      dialect: "sqlite",
+      storage: "db/database.sqlite"
     }
   }
 };
